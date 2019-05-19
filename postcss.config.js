434068d4672dcdf5c451966f24945c1662c1d957
Trung Nguyen <trung.nguyen@fluo.com>
2019-05-19 17:14:06 +0200

    chore(webpack): minify css for production

--- postcss.config.js
+++ postcss.config.js
@@ -2,10 +2,25 @@
 
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+const postcssPurgecss = require('@fullhuman/postcss-purgecss');
+const cssnano = require('cssnano');
+
+const purgecss = postcssPurgecss({
+  content: ['./src/**/*.html', './src/**/*.js'],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
 
 module.exports = {
   plugins: [
     tailwindcss('./tailwind.config.js'),
     autoprefixer,
+    ...(process.env.NODE_ENV === 'production'
+      ? [
+          purgecss,
+          cssnano({
+            preset: 'default',
+          }),
+        ]
+      : []),
   ],
-}
+};
